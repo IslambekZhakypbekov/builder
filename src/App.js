0f79d1f3e44a42a14e457360be68ce6115efae29
Islambek Zhakypbekov <islamzhakypbekov0@gmail.com>
2021-03-16 15:37:11 +0600

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Layout from "./Layout/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout>
        Restaurant table reservation via QR code by student Islam Zhakypbekov
-
+</Layout>
     </div>
   );
 }
---
+++ src/Layout/Layout.js
@@ -0,0 +1,16 @@
+import classes from "./Layout.module.css"
+import Drawer from "../Drawer/Drawer.js";
+import Toolbar from "../Toolbar/Toolbar.js";
+
+
+const Layout = (children) => {
+    return ( 
+        <div className={classes.Layout}>
+        <Toolbar/>
+        <Drawer/>
+        {children}
+        </div>
+     );
+}
+ 
+export default Layout;

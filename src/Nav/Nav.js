f5c1ee21e8f34575c121a16f8bf1a8f8dd909b82
Islambek Zhakypbekov <islamzhakypbekov0@gmail.com>
2021-03-16 15:47:00 +0600

    Added logo and Nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,9 @@
+import classes from "./Nav.module.css";
+
+const Nav = () => {
+    return ( 
+        <div className={classes.Nav}>Logo</div>
+     );
+}
+ 
+export default Nav;
--- src/Toolbar/Toolbar.js
+++ src/Toolbar/Toolbar.js
@@ -1,8 +1,15 @@
 import classes from "./Toolbar.module.css";
+import Logo from "../Logo/Logo.js";
+import Nav from "../Nav/Nav.js";
+
 
 const Toolbar = () => {
     return ( 
-        <div className={classes.Toolbar}>Toolbar</div>
+        <div className={classes.Toolbar}> 
+        <Logo/>
+        <Nav/>
+
+        oolbar</div>
      );
 }
  

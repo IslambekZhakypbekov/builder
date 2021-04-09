3c59d11d962aed35ded9b71c2f4b16ca362d3a96
Islambek Zhakypbekov <islamzhakypbekov0@gmail.com>
2021-04-09 16:13:22 +0600

    Delete

--- src/components/PizzaBuilder/PizzaIngredient/PizzaIngredient.js
+++ src/components/PizzaBuilder/PizzaIngredient/PizzaIngredient.js
@@ -18,25 +18,25 @@ const PizzaIngredient = ({ type, fixed }) => {
   //   yellowPepper: { backgroundImage: `url(${yellowPepperBackground})`, width: "40px", height: "40px" },
   // };
 
-  function getPosition(ingredientWidth) {
-    const pizzaDiameter = 380;
-    const pizzaRadius = pizzaDiameter / 2;
-    const ingredientRadius = parseInt(ingredientWidth) / 2;
+  // function getPosition(ingredientWidth) {
+  //   const pizzaDiameter = 380;
+  //   const pizzaRadius = pizzaDiameter / 2;
+  //   const ingredientRadius = parseInt(ingredientWidth) / 2;
 
-    const ingredientTop = Math.round(Math.random() * pizzaDiameter);
-    const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
+  //   const ingredientTop = Math.round(Math.random() * pizzaDiameter);
+  //   const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
 
-    const distance = Math.sqrt(
-      Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 2)
-    ) + ingredientRadius;
+  //   const distance = Math.sqrt(
+  //     Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 2)
+  //   ) + ingredientRadius;
 
-    return distance < pizzaRadius
-      ? {
-        top: ingredientTop - ingredientRadius,
-        left: ingredientLeft - ingredientRadius
-      }
-      : getPosition(ingredientWidth);
-  }
+  //   return distance < pizzaRadius
+  //     ? {
+  //       top: ingredientTop - ingredientRadius,
+  //       left: ingredientLeft - ingredientRadius
+  //     }
+  //     : getPosition(ingredientWidth);
+  // }
 
   // Get random position for this ingredient.
   // if (!fixed) {

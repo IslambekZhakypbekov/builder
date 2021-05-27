79de3db5837469488ab0c79be1bb23feb4f56bd2
Islambek Zhakypbekov <islamzhakypbekov0@gmail.com>
2021-05-27 20:47:49 +0600

    added placeholder

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -6,20 +6,22 @@ const CheckoutForm = ({ cancelCallback, submitCallback }) => {
     <form className={classes.CheckoutForm} onSubmit={submitCallback}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" required />
+        <input type="text" name="name" id="name" placeholder="Your name" required />
       </div>
       <div>
         <label htmlFor="address">Address</label>
-        <input type="text" name="address" id="address" required />
+        <input type="text" name="address" id="address" placeholder="Your addres" required />
       </div>
       <div>
         <label htmlFor="phone">Phone</label>
-        <input type="text" name="phone" id="phone" required pattern="0[0-9]{9}" />
+        <input type="text" name="phone" id="phone" placeholder="Your phone number" required pattern="0[0-9]{9}" />
       </div>
-      <Button>Checkout</Button>
-      <Button onClick={cancelCallback}>Cancel</Button>
+      <div className={classes.buttons}>
+        <Button>Checkout</Button>
+        <Button onClick={cancelCallback}>Cancel</Button>
+      </div>
     </form>
   );
 }
- 
+
 export default CheckoutForm;
